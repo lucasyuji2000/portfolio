058b818f65bb8cb45b0f599b89dee305834f0c06
Lucas Simon <lucasyuji2000@gmail.com>
2021-02-22 19:08:25 -0300

    :up: att sidebar e links

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var hamburguer = document.querySelector(".hamburguer");
 
-hamburguer.addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu");// toggle = alteração
+hamburguer.addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("show-menu");// toggle = alteração, poderia usar o add, mas teria q fzr outra funcao para fechar o sidebar, usando remove
+});
+
+var menuItem = document.querySelectorAll(".menu-link")
+
+menuItem.forEach(e => {
+    e.addEventListener("click", () => {
+        document.querySelector(".container").classList.remove("show-menu");
+    })
 });
